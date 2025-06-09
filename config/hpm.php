6d054fe46bf27b2aebb8ddd3c796be3702975fd9
Guenther Kron <kron@naturwelt.at>
2025-06-10 00:23:53 +0200

    Fixed all tests

--- config/hpm.php
+++ config/hpm.php
@@ -2,7 +2,7 @@
 
 // config for Itstudioat/Hpm
 return [
-    'version' => '0.0.4',
+    'version' => '0.0.5',
     'check_spatie_role' => false,
     'needed_role' => ['super_admin', 'hpm_admin'],
     'pv_homepage_path' => 'vendor/hpm/js/pages/pv_homepage/',
